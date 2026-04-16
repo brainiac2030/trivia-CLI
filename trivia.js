900c4f8903c143ff4cd39f358326d10ecbe1041c
brainiac2030 <isaackiambo@gmail.com>
2026-04-16 18:01:09 +0300

    setting up links between the program and the terminal and interactions between user and terminal

--- trivia.js
+++ trivia.js
@@ -1,3 +1,10 @@
 console.log("---Welcome to the Trivia game---");
 
-const readline = require('readline');
+const readline = require('readline'); // I need this for the back and forth with the user.
+
+const rl = readline.createInterface({     //.createInterface creates the link I need between this program and the terminal. To facilitate this back and forth with the user
+    input: process.stdin,  // yser will type this
+    output: process.stdout // this will get printed in the terminal
+});
+
+
